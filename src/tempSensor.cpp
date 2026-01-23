2f69b7cbcf86343f9bf0683e818378521c10c049
Evanlsmillan <evanschroedermil@ufl.edu>
2026-01-23 16:40:21 -0500

    added One wire library

---
+++ src/tempSensor.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
+#include "hardware/timer.h"
+
+#define UART0_TX_PIN 12
+#define UART0_RX_PIN 13
+
+bool tempSensor_INIT(){
+   
+}
